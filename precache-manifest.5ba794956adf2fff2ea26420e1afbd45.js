303afade9804cb0016e55aeb2a9f1b6a4b0fcec8
Andre-ARS <Andre-ARS@users.noreply.github.com>
2022-04-21 22:32:19 +0000

    deploy: 1a3534bf37f8ff4b4e9f506d8f52eb0b35e9bba6

---
+++ precache-manifest.5ba794956adf2fff2ea26420e1afbd45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c38bc2b691c01622e12458bb6f8d330",
+    "url": "/trybe-wallet/index.html"
+  },
+  {
+    "revision": "6966f135b360f24fbae1",
+    "url": "/trybe-wallet/static/css/main.e5e66784.chunk.css"
+  },
+  {
+    "revision": "c9946c20c53796f15e86",
+    "url": "/trybe-wallet/static/js/2.ad3f186d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-wallet/static/js/2.ad3f186d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6966f135b360f24fbae1",
+    "url": "/trybe-wallet/static/js/main.eaa42b2f.chunk.js"
+  },
+  {
+    "revision": "59d3c1ec903278203d08",
+    "url": "/trybe-wallet/static/js/runtime-main.2e2bea36.js"
+  },
+  {
+    "revision": "6aef8ac75414d190b9c5e47ae73f3f32",
+    "url": "/trybe-wallet/static/media/money-logo .6aef8ac7.png"
+  },
+  {
+    "revision": "16c2e08ef02234107fb4454563c6a316",
+    "url": "/trybe-wallet/static/media/money-logo-green.16c2e08e.png"
+  }
+]);
